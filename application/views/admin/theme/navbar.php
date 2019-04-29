18252ad67ec91435ee0a19eb346e0fd74e512dcc
rioandika <wakwaubahagia4@gmail.com>
2019-04-29 23:44:28 +0700

    tambah template

---
+++ application/views/admin/theme/navbar.php
@@ -0,0 +1,29 @@
+
+        <div class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    &nbsp;
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+                        <i class="icon-reorder shaded"></i></a><a class="brand" href="index.html"><i>logo banana</i></a>
+                    <div class="nav-collapse collapse navbar-inverse-collapse">
+                        <ul class="nav nav-icons">
+                            <li>
+                                Lumajang, Jawa Timur 67371 <br>
+                                Telegram : t.me/bananahijab <br>
+                                Fanspage : Banana Hijab <br> &nbsp;
+                            </li>
+                        </ul>
+                        <ul class="nav pull-right">
+                            <form class="navbar-search pull-left input-append" action="#">
+                                <input type="text" class="span3">
+                                <button class="btn" type="button">
+                                    <i class="icon-search"></i>
+                                </button>
+                            </form>
+                        </ul>
+                    </div>
+                    <!-- /.nav-collapse -->
+                </div>
+            </div>
+            <!-- /navbar-inner -->
+        </div>
---
+++ application/views/admin/vIndex.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once('theme/head.php')?>
+    <body>
+        <?php require_once('theme/navbar.php')?>
+        <!-- /navbar -->
+        <div class="wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="span3">
+                        <?php require_once('theme/menu.php')?>
+                    </div>
+                    <!--/.span3-->
+                    <div class="span9">
+                        <div class="content">
+                            <div class="btn-controls">
+                                <div class="module">
+                                    <div class="module-body">
+                                        <div class="chart inline-legend grid">
+                                            <div id="" class="" style="height: 200px">
+                                                logo always with you
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="btn-box-row row-fluid">
+                                    <a href="#" class="btn-box big span3">
+                                        <i class="" style="height: 200px">Gambar produk</i>
+                                    </a>
+                                    <a href="#" class="btn-box big span3">
+                                        <i class="" style="height: 200px">Gambar produk</i>
+                                    </a>
+                                    <a href="#" class="btn-box big span3">
+                                        <i class="" style="height: 200px">Gambar produk</i>
+                                    </a>
+                                    <a href="#" class="btn-box big span3">
+                                        <i class="" style="height: 200px">Gambar produk</i>
+                                    </a>
+                                </div>
+                            </div>
+                            <!--/#btn-controls-->
+                            <!--/.module-->
+                            <div class="module hide">
+                                <div class="module-head">
+                                    <h3>
+                                        Adjust Budget Range</h3>
+                                </div>
+                                <div class="module-body">
+                                    <div class="form-inline clearfix">
+                                        <a href="#" class="btn pull-right">Update</a>
+                                        <label for="amount">
+                                            Price range:</label>
+                                        &nbsp;
+                                        <input type="text" id="amount" class="input-" />
+                                    </div>
+                                    <hr />
+                                    <div class="slider-range">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--/.content-->
+                    </div>
+                    <!--/.span9-->
+                </div>
+            </div>
+            <!--/.container-->
+        </div>
+        <!--/.wrapper-->
+        <?php require_once('theme/footerandscript.php')?>
+      
+    </body>
